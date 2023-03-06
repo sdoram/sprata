1cf62686deb6d1556128cb34696444fd38923d1f
Seman Kim <sdoramkim@gmail.com>
2023-03-06 19:13:12 +0900

    Update app.py
    youtube도 아니고 naver에서 내가 원하던 걸 긁어온 것도 아니지만 오류 없이 정상적으로 텍스트를 긁어왔다는 게 감동이다.

--- bs4prac/app.py
+++ bs4prac/app.py
@@ -1,46 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import os
 import chromedriver_autoinstaller as AutoChrome
 import shutil
+import time
 
-# def chromedriver_update(): # 크롬드라이버 자동 업데이트
-#     chrome_ver = AutoChrome.get_chrome_version().split('.')[0] # 현재 크롬 버전 확인
+def chromedriver_update(): # 크롬드라이버 자동 업데이트
+    chrome_ver = AutoChrome.get_chrome_version().split('.')[0] # 현재 크롬 버전 확인
+    current_list = os.listdir(os.getcwd()) # 현재 디렉토리 파일,폴더 리스트 
     
-
-#     current_list = os.listdir(os.getcwd()) # 현재 디렉토리 파일,폴더 리스트 
-#     chrome_list = []
-
-#     for i in current_list:
-#         path = os.path.join(os.getcwd(),i) # 현재 디렉토리에 파일,폴더를 더해 주소를 만듦
-#         if os.path.isdir(path): # 폴더만 추린다.
-#             # print(f'폴더명 : {i} / 내용물 {os.listdir(path)}') # 그 폴더의 내용물을 리스트로 출력한다.
-#             if 'chromedriver.exe' in os.listdir(path): #'chromedriver.exe'가 해당 폴더에 있는지 확인
-#                 chrome_list.append(i)
+    chrome_list = []
+    for i in current_list:
+        path = os.path.join(os.getcwd(),i) # 현재 디렉토리에 파일,폴더를 더해 주소를 만듦
+        if os.path.isdir(path): # 폴더만 추린다.
+            if 'chromedriver.exe' in os.listdir(path): #'chromedriver.exe'가 해당 폴더에 있는지 확인
+                chrome_list.append(i)
                 
-#     old_version = list(set(chrome_list) - set([chrome_ver])) # 크롬드라이버가 있는 폴더중에 최신폴더 제외
+    old_version = list(set(chrome_list) - set([chrome_ver])) # 크롬드라이버가 있는 폴더중에 최신폴더 제외
     
-#     for i in old_version:
-#         path = os.path.join(os.getcwd(),i) # 구버전 폴더 경로
-#         shutil.rmtree(path)                    # 구버전 폴더 삭제
+    for i in old_version:
+        path = os.path.join(os.getcwd(),i) # 구버전 폴더 경로
+        shutil.rmtree(path)                    # 구버전 폴더 삭제
     
-#     if not chrome_ver in current_list:     # not 최신버전 in 현재 경로:
-#         AutoChrome.install(True)           # 크롬 드라이버 설치
-#     else: pass                             # pass
+    if not chrome_ver in current_list:     # not 최신버전 in 현재 경로:
+        AutoChrome.install(True)           # 크롬 드라이버 설치
+    else: pass                             # pass
+     
+def selenium_test():
+    chrome_ver = AutoChrome.get_chrome_version().split('.')[0]
+    path = os.path.join(os.getcwd(),chrome_ver)
+    path = os.path.join(path,'chromedriver.exe')
+    URL = 'https://www.naver.com/'
+    driver = webdriver.Chrome(str(path))
+    driver.get(url=URL)
+    news = driver.find_element(By.CLASS_NAME,"title")
+    print(news.text)
     
-       
-# def selenium_test():
-    # options = webdriver.ChromeOptions()
-    # options.add_experimental_option("detach", True)
-    # driver = webdriver.Chrome("./chromedriver.exe", options=options)
-    # chrome_ver = AutoChrome.get_chrome_version().split('.')[0]
-    # path = os.path.join(os.getcwd(),chrome_ver)
-    # path = os.path.join(path,'chromedriver.exe')
-    # print(path)
-    # URL = 'https://www.google.co.kr/'
-    # driver = webdriver.Chrome(str(path))
-    # driver.get(url=URL)
     
-    # while(True):
+    
+    # while(True): # 브라우저 닫힘 방지
     #     pass
+    time.sleep(3) # 3초 뒤에 닫힘
+    
+chromedriver_update() # 크롬드라이버 자동 업데이트
+selenium_test()
