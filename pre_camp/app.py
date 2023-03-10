34fedb5c726d9337ae2466516f15df3389f3a7d9
Se_Man_Kim <108051577+sdoram@users.noreply.github.com>
2023-03-10 22:16:53 +0900

    Update app.py

--- pre_camp/app.py
+++ pre_camp/app.py
@@ -7,8 +7,6 @@ import requests
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sdoram:RLATPAKS3@sparta.utt9hlo.mongodb.net/sparta?retryWrites=true&w=majority')
-db = client.dbsparta1
 
 
 @app.route('/')
